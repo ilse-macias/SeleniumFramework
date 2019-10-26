5beef3779d59999fa3df9cdc4d7934b4eaa60c62
Ilse Macías <ilse.macias@gmail.com>
2019-10-26 12:55:03 -0700

    Hello World
    Start a simple test case.

---
+++ SeleniumFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6d6dfbd-e5b1-4e9c-9755-ba7a9dc991f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumFramework/TestBase.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System;
+
+namespace SeleniumFramework
+{
+    public class TestBase
+    {
+        protected IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+
+            _driver.Manage().Cookies.DeleteAllCookies();
+            _driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            Console.WriteLine("URL opened.");
+
+            _driver.Manage().Window.Maximize();
+            Console.WriteLine("Windows Maximized.");
+            Thread.Sleep(3000);
+
+        }
+
+        [Test]
+        public void SearchElement()
+        {
+            IWebElement search = _driver.FindElement(By.Id("search_query_top"));
+            search.SendKeys("t-shirt");
+            Thread.Sleep(3000);
+            Console.WriteLine("Element located.");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Close();
+            _driver.Quit();
+            Console.WriteLine("Close Browser.");
+        }
+    }
+}
